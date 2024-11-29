f4f1579d7918aed1ca4ffe1741d6433f4434ae12
gwangeunahn <gwangeunahn@handong.ac.kr>
2024-11-29 19:53:58 +0900

    #005

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -7,13 +7,13 @@ export default function Header() {
       <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none col-3">
         <span className="fs-2">WanJol</span>
       </a>
-      <ul className="nav nav-pills col-6">
-        <li className="nav-item"><a href="/" className="nav-link">서비스 정보</a></li>
+      <ul className="nav nav-pills col-7">
+        <li className="nav-item"><a href="/" className="nav-link">서비스 소개</a></li>
         <li className="nav-item"><a href="/" className="nav-link">졸업 요건</a></li>
-        <li className="nav-item"><a href="/" className="nav-link">직업 찾기</a></li>
+        <li className="nav-item"><a href="/" className="nav-link">직업 탐색</a></li>
         <li className="nav-item"><a href="/" className="nav-link">나의 정보</a></li>
       </ul>
-      <div className="nav-item col-2">
+      <div className="nav-item col-1">
         <a href="/" className="btn btn-primary rounded-pill" aria-current="page">로그인</a>
       </div>
     </header>
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -4,21 +4,21 @@ export default function Home() {
   return (
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
         <div class="col-md-6 p-lg-5 mx-auto my-5">
-        <h1 class="display-3 fw-bold">Designed for engineers</h1>
-        <h3 class="fw-normal text-muted mb-3">Build anything you want with Aperture</h3>
+        <h1 class="display-3 fw-bold">완졸(WanJol)</h1>
+        <h3 class="fw-normal text-muted mb-3">"완벽한 졸업"의 줄임말로, 한국어로 간결하고 친근한 느낌</h3>
         <div class="d-flex gap-3 justify-content-center lead fw-normal">
             <a class="icon-link" href="#">
-            Learn more
+                선택1
             <svg class="bi"></svg>
             </a>
             <a class="icon-link" href="#">
-            Buy
+                선택2
             <svg class="bi"></svg>
             </a>
         </div>
         </div>
-        <div class="product-device shadow-sm d-none d-md-block"></div>
-        <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
   </div>
   )
 }
