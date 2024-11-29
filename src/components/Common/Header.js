0e82f24397491634b684485b4dcc2942d716850e
gwangeunahn <gwangeunahn@handong.ac.kr>
2024-11-29 19:40:26 +0900

    #004

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <header className="d-flex flex-wrap justify-content-center py-3">
       <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none col-3">
         <span className="fs-2">WanJol</span>
       </a>
